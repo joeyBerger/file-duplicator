065cffa05e17108d901e9ae61b958a57ae4f4b3a
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-02-04 20:50:29 -0800

    added more input widgets

--- drag_drop.py
+++ drag_drop.py
@@ -13,35 +13,58 @@ class MainWidget(QMainWindow):
         self.setFixedSize(500, 300)       
         self.setAcceptDrops(True)
 
-        widget_buffer = 50
+        widget_buffer = (30,50)
         text_field_size = (350,30)
 
         self.file_display_text = QTextEdit(self)
         self.file_display_text.setReadOnly(True)
-        self.file_display_text.move(int(self.rect().width()/2-text_field_size[0]/2),int(widget_buffer-text_field_size[1]/2))
+        self.file_display_text.move(int(self.rect().width()/2-text_field_size[0]/2),int(widget_buffer[1]-text_field_size[1]/2))
         self.file_display_text.resize(text_field_size[0],text_field_size[1])
+        container_dimensions = (self.rect().width(),self.rect().height())
 
         self.file_display_label = QLabel('Left', self)
         self.file_display_label.resize(200,30)
-        self.file_display_label.move(int(self.rect().width()/2-self.file_display_label.rect().width()/2),int(widget_buffer-text_field_size[1]/2-25))
+        self.file_display_label.move(int(container_dimensions[0]/2-self.file_display_label.rect().width()/2),int(widget_buffer[1]-text_field_size[1]/2-25))
         self.file_display_label.setText("File To Duplicate (Drag File):")
 
+        text_field_size = (150,30)
+
+        self.added_text = QTextEdit(self)
+        self.added_text.move(widget_buffer[0],int(container_dimensions[1]/2-text_field_size[1]/2))
+        self.added_text.resize(text_field_size[0],text_field_size[1])
+        self.added_text_label = QLabel('Center', self)
+        self.added_text_label.resize(120,30)
+        self.added_text_label.move(self.added_text.pos().x(),int(container_dimensions[1]/2-text_field_size[1]/2-25))
+        self.added_text_label.setText("Text To Add:")
+
         text_field_size = (60,30)
-        self.copy_amount_text = QTextEdit(self)
-        self.copy_amount_text.move(int(self.rect().width()/2-text_field_size[0]/2),int(self.rect().height()/2-text_field_size[1]/2))        
+
+        self.starting_number_text = QTextEdit(self)
+        self.starting_number_text.move(container_dimensions[0]/2-20,int(container_dimensions[1]/2-text_field_size[1]/2))
+        self.starting_number_text.resize(text_field_size[0],text_field_size[1])
+        self.starting_number_text.setPlainText("1")
+        self.starting_number_text.textChanged.connect(self.onAmountChange)
+
+        self.starting_number_label = QLabel('Center', self)
+        self.starting_number_label.resize(120,30)
+        self.starting_number_label.move(self.starting_number_text.pos().x(),int(container_dimensions[1]/2-text_field_size[1]/2-25))
+        self.starting_number_label.setText("Starting Number:")
+
+        self.copy_amount_text = QTextEdit(self)  
+        self.copy_amount_text.move(int(container_dimensions[0]-(widget_buffer[0]*2+text_field_size[0])),int(container_dimensions[1]/2-text_field_size[1]/2))        
         self.copy_amount_text.resize(text_field_size[0],text_field_size[1])
         # self.copy_amount_text.setAlignment(QtCore.Qt.AlignRight)
         # self.copy_amount_text.setHtml("<p align=\"right\">This paragraph is right aligned")
+
         self.copy_amount_label = QLabel('Center', self)
-        self.copy_amount_label.move(int(self.rect().width()/2-self.copy_amount_text.rect().width()/2),int(self.rect().height()/2-text_field_size[1]/2-25))
-        self.copy_amount_label.setText("Duplication Amount:")
+        self.copy_amount_label.resize(120,30)
+        self.copy_amount_label.move(self.copy_amount_text.pos().x(),int(container_dimensions[1]/2-text_field_size[1]/2-25))
+        self.copy_amount_label.setText("Duplications:")
         self.copy_amount_text.setPlainText("1")
-        self.copy_amount_text.textChanged.connect(self.onAmountChange) 
-
-
-
+        self.copy_amount_text.textChanged.connect(self.onAmountChange)
+        
         self.duplicate_button = QPushButton('Duplicate', self)
-        self.duplicate_button.move(int(self.rect().width()/2-self.duplicate_button.rect().width()/2),int(self.rect().height()-widget_buffer-self.duplicate_button.rect().height()/2))
+        self.duplicate_button.move(int(container_dimensions[0]/2-self.duplicate_button.rect().width()/2),int(container_dimensions[1]-widget_buffer[1]-self.duplicate_button.rect().height()/2))
         self.duplicate_button.clicked.connect(self.onClick)
         
     def onClick(self):
